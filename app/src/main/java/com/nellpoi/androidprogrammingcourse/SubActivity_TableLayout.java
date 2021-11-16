b9c0bbe7783029069fb6dc759762071bce118165
Jun <NellPoi@users.noreply.github.com>
2021-11-16 20:40:27 +0800

    Initial commit 创建了LinearLayout

---
+++ app/src/main/java/com/nellpoi/androidprogrammingcourse/SubActivity_TableLayout.java
@@ -0,0 +1,14 @@
+package com.nellpoi.androidprogrammingcourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivity_TableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_table_layout);
+    }
+}
